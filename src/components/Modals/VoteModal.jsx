c6130c318bb1f35dd3de1e81dfe8959b14b6c715
tcongminh04 <tcongminh1604@gmail.com>
2025-09-25 18:12:13 +0700

    refactor: rename createdBy field to creatorId in VoteModal component

--- src/components/Modals/VoteModal.jsx
+++ src/components/Modals/VoteModal.jsx
@@ -65,7 +65,7 @@ const VoteModal = () => {
         title: form.title,
         options: validOptions,
         roomId: selectedRoom.id,
-        createdBy: uid,
+        creatorId: uid,
         creatorName: displayName,
       };
 
